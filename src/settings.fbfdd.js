275e0b4fa74a534fc9b25fb8a698efb6f1d85764
ゆきみぽんず <blackstartsubaki@gmail.com>
2021-03-17 21:17:55 +0900

    Revert "Add files via upload"
    This reverts commit 5e60f5bca4a022edd7f1de36fa57c9108f2447f8.

--- src/settings.fbfdd.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",main:"9a1d1"}};
